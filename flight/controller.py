866f33d54be589e3669a8641555271bbee1f181c
s-chandr <chandrasayan15@gmail.com>
2023-09-05 12:05:03 +0530

    login-signup

--- flight/controller.py
+++
@@ -1,9 +0,0 @@
-from application import  mongo
-flights_collection = mongo['flights_collection']
-
-def book_seats( flight_number , taken ) :
-    result = flights_collection.update_one(
-        {"flight_number": flight_number},
-        {"$inc": {"seats": -taken}}
-    )
-    return True if result.modified_count == 1 else False
--- wsgi.py
+++ wsgi.py
@@ -3,15 +3,11 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.exceptions import NotFound
 from application import app,ping_app
 from user.views import User
-from flight.views import Flight
-from booking.views import Booking
-
 
 app.register_blueprint(User)
-app.register_blueprint(Flight)
-app.register_blueprint(Booking)
 
-application = DispatcherMiddleware(ping_app, {"/indigos": app})
+
+application = DispatcherMiddleware(ping_app, {"/api": app})
 
 if __name__ == "__main__":
     run_simple("0.0.0.0", 8000, application, use_reloader=True, threaded=True)
